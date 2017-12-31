9bbf6496d95be81e0b86b009082c68f9899483a2
Şevval MEHDER <smehder@gtu.edu.tr>
2017-12-31 16:46:07 +0300

    New method function

--- ZabbixApi.java
+++ ZabbixApi.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 
 public class ZabbixApi {
 
-
+    //
+    // HOST FUNCTIONS
+    //
 
     //This method allows to create new hosts.
     public static JSONObject createHost(String hostName, JSONObject interfaces,  JSONObject groups, String auth) throws JSONException {
@@ -16,7 +18,7 @@ public class ZabbixApi {
         jo.put("interfaces", interfaces);
         jo.put("groups", groups);
 
-        return method("host.create", jo, auth);
+       return method("host.create", jo, null, auth);
     }
 
     //This method allows to delete hosts.
@@ -24,7 +26,7 @@ public class ZabbixApi {
 
         JSONObject jo = new JSONObject(ids);
 
-        return method("host.delete", jo, auth);
+        return method("host.delete", jo, null, auth);
     }
 
 
@@ -35,7 +37,7 @@ public class ZabbixApi {
         jo.put("host", host);
         jo.put("nodeids", nodeids);
 
-        return method("host.exists", jo, auth);
+         return method("host.exists", jo, null, auth);
     }
 
     //The method allows to retrieve hosts according to the given parameters.
@@ -46,7 +48,7 @@ public class ZabbixApi {
         jo2.put("host", hostnames);
         jo.put("filter", jo2);
 
-        return method("host.get", jo, auth);
+        return method("host.get", jo, null, auth);
     }
 
     //This method allows to update existing hosts.
@@ -56,16 +58,20 @@ public class ZabbixApi {
         jo.put("hostid", hostid);
         jo.put("status", status);
 
-        return method("host.update", jo, auth);
+        return method("host.update", jo, null, auth);
     }
-
+    
+    //
+    // HOST GROUP FUNCTIONS
+    //
+    
     //This method allows to create new host groups.
     public static JSONObject createHostGroup(String name, String auth) throws JSONException {
 
         JSONObject jo = new JSONObject();
         jo.put("name", name);
 
-        return method("hostgroup.update", jo, auth);
+       return method("hostgroup.update", jo, null, auth);
     }
 
     //This method allows to delete host groups.
@@ -73,7 +79,7 @@ public class ZabbixApi {
 
         JSONObject jo = new JSONObject(ids);
 
-        return method("hostgroup.delete", jo, auth);
+        return method("hostgroup.delete", jo, null, auth);
     }
 
     //This method checks if at least one host group that matches the given filter criteria exists.
@@ -83,7 +89,7 @@ public class ZabbixApi {
         jo.put("name", host);
         jo.put("nodeids", nodeids);
 
-        return method("hostgroup.exists", jo, auth);
+        return method("hostgroup.exists", jo, null, auth);;
     }
 
     //The method allows to retrieve host groups according to the given parameters.
@@ -95,7 +101,7 @@ public class ZabbixApi {
         jo2.put("name", hostnames);
         jo.put("filter", jo2);
 
-        return method("hostgroup.get", jo, auth);
+        return method("hostgroup.get", jo, null, auth);
     }
 
     //This method allows to update existing hosts.
@@ -105,10 +111,13 @@ public class ZabbixApi {
         jo.put("groupid", groupid);
         jo.put("name", name);
 
-        return method("hostgroup.update", jo, auth);
+        return method("hostgroup.update", jo, null, auth);
     }
 
-
+    //
+    // GRAPH FUNCTIONS
+    //
+    
     //This method allows to create new graphs.
     public static JSONObject createGraph(String name, int width, int height, JSONObject[] gitems, String auth) throws JSONException {
 
@@ -118,14 +127,14 @@ public class ZabbixApi {
         jo.put("height", height);
         jo.put("gitems", gitems);
 
-        return method("graph.create", jo, auth);
+        return method("graph.create", jo, null, auth);
     }
 
     public static JSONObject deleteGraph(String[] graphids, String auth) throws JSONException {
 
         JSONObject jo = new JSONObject(graphids);
 
-        return method("graph.delete", jo, auth);
+        return method("graph.delete", jo, null, auth);
     }
 
     //This method checks if at least one graph that matches the given filter criteria exists.
@@ -135,7 +144,7 @@ public class ZabbixApi {
         jo.put("name", name);
         jo.put("host", host);
 
-        return method("graph.exists", jo, auth);
+        return method("graph.exists", jo, null, auth);
     }
 
     //This method allows to update existing graphs.
@@ -143,11 +152,12 @@ public class ZabbixApi {
 
         JSONObject jo = new JSONObject(gitems);
 
-        return method("graph.update", jo, auth);
+        return method("graph.update", jo, null, auth);
     }
 
 
-    public static JSONObject createItem(String name, String key, String hostID, String type, String value_type, String interfaceid, String[] applications, String delay String auth) throws JSONException {
+    public static JSONObject createItem(String name, String key, String hostID, String type, String value_type, String interfaceid, 
+                                        String[] applications, String delay, String auth) throws JSONException {
 
     JSONObject jo = new JSONObject();
     jo.put("name", name);
@@ -159,11 +169,11 @@ public class ZabbixApi {
     jo.put("delay", delay);
 
 
-    return method("item.create", jo, auth);
+    return method("item.create", jo, null, auth);
 
     }
 
-    public static JSONObject getItem(String output, String hostids, String search, String sortfield
+    public static JSONObject getItem(String output, String hostids, String search, String sortfield,
     String auth) throws JSONException {
 
     JSONObject jo = new JSONObject();
@@ -172,14 +182,13 @@ public class ZabbixApi {
     jo.put("search", search);
     jo.put("sortfield", sortfield);
 
-    return method("item.get", jo, auth);
+    return method("item.get", jo, null, auth);
 
     }
 
-
     public static JSONObject deleteItem(String [] items, String auth) throws JSONException {
 
-    return method("item.delete", items, auth);
+    return method("item.delete", null, items, auth);
 
     }
 
@@ -189,21 +198,22 @@ public class ZabbixApi {
     jo.put("itemid", itemid);
     jo.put("status", status);
 
-    return method("item.update", jo, auth);
+    return method("item.update", jo, null, auth);
 
     }
+    
+    //
+    // USER FUNCTIONS
+    //
 
-
-
-
-    public static JSONObject authenticateUser(String alias, String passwd, String auth) throws JSONException {
+    /* public static JSONObject authenticateUser(String alias, String passwd, String auth) throws JSONException {
 
         JSONObject jo = new JSONObject();
         jo.put("alias", alias);
         jo.put("passwd", passwd);
 
         return method("user.create", jo,  auth);
-    }
+    } */
 
     public static JSONObject createUser(String alias, String passwd, String auth) throws JSONException {
 
@@ -211,11 +221,9 @@ public class ZabbixApi {
         jo.put("alias", alias);
         jo.put("passwd", passwd);
 
-        return method("user.create", jo,  auth);
+        return method("user.create", jo, null, auth);
     }
 
-
-/*
     public static JSONObject deleteUser(String[] userIds, String auth) throws JSONException {
 
         JSONObject jo = new JSONObject();
@@ -224,9 +232,8 @@ public class ZabbixApi {
 
         }
 
-        return method("user.create", jo,  auth);
+        return method("user.create", jo, null, auth);
     }
-*/
 
     //When using this method, you also need to do user.logout to
     // prevent the generation of a large number of open session records.
@@ -236,7 +243,7 @@ public class ZabbixApi {
         jo.put("user", user);
         jo.put("password", password);
 
-        return method("user.login", jo);
+        return method("user.login", jo, null, null);
     }
 
     public static JSONObject loginUser(String user, String password, Boolean userData) throws JSONException {
@@ -246,15 +253,15 @@ public class ZabbixApi {
         jo.put("password", password);
         jo.put("userData", userData);
 
-        return method("user.login", jo);
+        return method("user.login", jo, null, null);
     }
 
-   /* public static JSONObject logoutUser(String[] id, Boolean userData) throws JSONException {
+    public static JSONObject logoutUser(String[] id, Boolean userData) throws JSONException {
 
         JSONObject jo = new JSONObject();
 
-        return method("user.logout", jo);
-    }*/
+        return method("user.logout", jo, null, null);
+    }
 
 
     public static JSONObject getUser(String output, String auth) throws JSONException {
@@ -262,7 +269,7 @@ public class ZabbixApi {
         JSONObject jo = new JSONObject();
         jo.put("output", output);
 
-        return method("user.get", jo,  auth);
+        return method("user.get", jo, null, auth);
     }
 
     public static JSONObject updateUser(String userid, String name, String surname, String auth) throws JSONException {
@@ -272,7 +279,7 @@ public class ZabbixApi {
         jo.put("name", name);
         jo.put("surname", surname);
 
-        return method("user.update", jo, auth);
+        return method("user.update", jo, null, auth);
     }
 
     //This method allows to update the currently logged in user.
@@ -282,62 +289,33 @@ public class ZabbixApi {
         jo.put("name", name);
         jo.put("lastname", lastname);
 
-        return method("user.updateprofile", jo, auth);
+        return method("user.updateprofile", jo, null, auth);
     }
-
-
-
-    private static JSONObject method(String methodName, JSONObject params, String auth) throws JSONException {
+    
+    
+    // method(String methodName, JSONOBject json_param, String[] str_param, String auth)
+    private static JSONObject method(String methodName, JSONObject json_param, String[] str_param, String auth) throws JSONException {
 
         JSONObject jsonObject = new JSONObject();
 
         jsonObject.put("jsonrpc", "2.0");
         jsonObject.put("method", methodName);
-        jsonObject.put("params", params);
-        jsonObject.put("auth", auth);
+
+        // Params
+        if (json_param == null)
+            jsonObject.put("params", str_param);
+        else
+            jsonObject.put("params", json_param);
+
+        if (auth != null)
+            jsonObject.put("auth", auth);
+
+        // id
         jsonObject.put("id", "1");
 
         return jsonObject;
     }
-
-    private static JSONObject method(String methodName, JSONObject params) throws JSONException {
-
-        JSONObject jsonObject = new JSONObject();
-
-        jsonObject.put("jsonrpc", "2.0");
-        jsonObject.put("method", methodName);
-        jsonObject.put("params", params);
-        jsonObject.put("id", "1");
-
-        return jsonObject;
-    }
-
-    private static JSONObject method(String methodName, String[] params, String auth) throws JSONException {
-
-        JSONObject jsonObject = new JSONObject();
-
-        jsonObject.put("jsonrpc", "2.0");
-        jsonObject.put("method", methodName);
-        jsonObject.put("params", params);
-        jsonObject.put("auth", auth);
-        jsonObject.put("id", "1");
-
-        return jsonObject;
-    }
-
-    private static JSONObject method(String methodName, String[] params) throws JSONException {
-
-        JSONObject jsonObject = new JSONObject();
-
-        jsonObject.put("jsonrpc", "2.0");
-        jsonObject.put("method", methodName);
-        jsonObject.put("params", params);
-        jsonObject.put("id", "1");
-
-        return jsonObject;
-    }
-
-
+    
     public static void main(String args[]) throws IOException, JSONException {
 
         JSONObject jsonParam = new JSONObject();
